abf09605c3a0ed420f6a1f89264610fbe9d955ba
Im Gyurim <rdsall123@naver.com>
2023-06-30 18:15:59 +0900

    f

--- 41.svg/js/study6.js
+++ 41.svg/js/study6.js
@@ -1,35 +1,52 @@
 $(document).ready(function(){
 
-    let pathLength = $("#progress path").get(0).getTotalLength()
-    $("#progress path").css("stroke-dasharray",pathLength)
-    $("#progress path").css("stroke-dashoffset",pathLength)
 
-    $(window).scroll(function(){
-        let winTop = $(window).scrollTop()
-        let fixedTop = $(".fixedFrame").offset().top
-        let distance = winTop-fixedTop
-        let movingAreaHeight = $(".movingArea").height()
+  let pathLength = $("#progress path").get(0).getTotalLength()
+  $("#progress path").css("stroke-dasharray",pathLength)
+  $("#progress path").css("stroke-dashoffset",pathLength)
 
-        if(distance<0){
-            $(".movingArea").css("position","absolute")
-            $(".movingArea").css("left","0")
-            $(".movingArea").css("top","0")
-        }
-        if(distance>=0 && distance <= 8000-movingAreaHeight){
-            $(".movingArea").css("position","fixed")
-            $(".movingArea").css("left","0")
-            $(".movingArea").css("top","0")
-            let percent = (distance/(8000-movingAreaHeight)*100)
-            $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
-            $(".per").text(Math.round(percent)+"%")
-        }
-        if(distance>8000-movingAreaHeight){
-            $(".movingArea").css("position","absolute")
-            $(".movingArea").css("left","0")
-            $(".movingArea").css("top",(8000-movingAreaHeight)+"px")
-        }
-    })
     
-    
+  $(window).scroll(function(){
+      let winTop = $(window).scrollTop()
+      let fixedTop = $(".fixedFrame").offset().top
+      let distance = winTop-fixedTop
+      let movingAreaHeight = $(".movingArea").height()        
+      
+      if(distance<0){
+
+          $(".movingArea").css("position","absolute")
+          $(".movingArea").css("left","0")
+          $(".movingArea").css("top","0")
+
+      }
+      if(distance>=0 && distance<=8000-movingAreaHeight){
+          $(".movingArea").css("position","fixed")
+          $(".movingArea").css("left","0")
+          $(".movingArea").css("top","0")
+          let percent = (distance/(8000-movingAreaHeight)*100)     //0~100까지의 수        
+          $("#progress path").css("stroke-dashoffset",pathLength-(pathLength*percent/100))
+          $(".per").text(Math.round(percent)+"%")
+
+          let percent2 = (distance/(8000-movingAreaHeight)*87.5) //0~87.5까지의 수
+          $(".train").css("transform","translateX(-"+percent2+"%)")
+
+          
+      }
+      if(distance>8000-movingAreaHeight){
+          $(".movingArea").css("position","absolute")
+          $(".movingArea").css("left","0")
+          $(".movingArea").css("top",(8000-movingAreaHeight)+"px")
+
+      }  
+      
+      
+
+      
+
+  })
 })
+
+
+
+
 
