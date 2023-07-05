ffab7646b2489dc21af9c7490f90237b148c68b8
Im Gyurim <rdsall123@naver.com>
2023-07-05 18:07:23 +0900

    w

--- 프로필/js/profile.js
+++ 프로필/js/profile.js
@@ -7,58 +7,86 @@ $(document).ready(function(){
     // pathLength - (pathLength*0.9)
     // $(".c1").css("stroke-dashoffset",pathLength - (pathLength*0.9))
 
-  $(".btn").click(function(){
+    $(".btn").click(function(){
 
-      if($(".skill_List").hasClass("on")==false){
-          // skill클래스에 on클래스가 없다면
-          $(".skill_List").addClass("on")
-          $(".skill_List>li").each(function(){
-              let list = $(this)
-              let percent = $(this).find(".per").text()
-              let count = 0
-              let circle = $(this).find("circle")
+        if($(".skill_List").hasClass("on")==false){
+            // skill클래스에 on클래스가 없다면
+            $(".skill_List").addClass("on")
+            $(".skill_List>li").each(function(){
+                let list = $(this)
+                let percent = $(this).find(".per").text()
+                let count = 0
+                let circle = $(this).find("circle")
 
-              let timer = setInterval(function(){
-                  count++
-                  list.find(".per").text(count+"%")
-                  circle.css("stroke-dashoffset",314-(314*(count/100)))
-                  if(count>=percent){
-                      clearInterval(timer)
-                  }
-              },10)
-          })
-      }
-  })
+                let timer = setInterval(function(){
+                    count++
+                    list.find(".per").text(count+"%")
+                    circle.css("stroke-dashoffset",314-(314*(count/100)))
+                    if(count>=percent){
+                        clearInterval(timer)
+                    }
+                },10)
+            })
+        }
+    })
 
-  $(window).scroll(function(){
-      let a = $(window).scrollTop()
-      let b = $(".skill_List").offset().top
-      let c = $(window).height()*0.5
+    $(window).scroll(function(){
+        let a = $(window).scrollTop()
+        let b = $(".skill_List").offset().top
+        let c = $(window).height()*0.5
 
-      if(a+c>=b){
-          if($(".skill_List").hasClass("on")==false){
-              // skill클래스에 on클래스가 없다면
-              $(".skill_List").addClass("on")
-              $(".skill_List>li").each(function(){
-                  let list = $(this)
-                  let percent = $(this).find(".per").text()
-                  let count = 0
-                  let circle = $(this).find("circle")
-  
-                  let timer = setInterval(function(){
-                      count++
-                      list.find(".per").text(count+"%")
-                      circle.css("stroke-dashoffset",314-(314*(count/100)))
-                      if(count>=percent){
-                          clearInterval(timer)
-                      }
-                  },10)
-              })
-          }
-      }
+        if(a+c>=b){
+            if($(".skill_List").hasClass("on")==false){
+                // skill클래스에 on클래스가 없다면
+                $(".skill_List").addClass("on")
+                $(".skill_List>li").each(function(){
+                    let list = $(this)
+                    let percent = $(this).find(".per").text()
+                    let count = 0
+                    let circle = $(this).find("circle")
+    
+                    let timer = setInterval(function(){
+                        count++
+                        list.find(".per").text(count+"%")
+                        circle.css("stroke-dashoffset",314-(314*(count/100)))
+                        if(count>=percent){
+                            clearInterval(timer)
+                        }
+                    },10)
+                })
+            }
+        }
 
-  })
-  
+        
+    })
+
+    $(".tab>li").click(function(){
+        $(".tab>li").removeClass("on")
+        $(this).addClass("on")
+
+        $(".train>li").removeClass("on")
+        $(".train>li".)eq(count).addClass("on")
+    })
+
+    // let count = 0;
+    // $(".prevbtn").click(function(e){
+        
+    //     count++;
+    //     if(count>7){
+    //         count=0
+    //     }
+    //     e.preventDefault()
+    //     $(".train>li").removeClass("on")
+    //     $(".train>li").eq(count).addClass("on")
+    // })
+    // $(".nextbtn").click(function(e){
+        
+    //     count--;
+    //     if(count<0){
+    //         count=7
+    //     }
+    //     e.preventDefault()
+    //     $(".train>li").removeClass("on")
+    //     $(".train>li").eq(count).addClass("on")
+    // })
 })
-
- 
