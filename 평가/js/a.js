413c40ba2bb6b99d87661f64f484a1b5893fcfa4
Im Gyurim <rdsall123@naver.com>
2023-07-07 10:59:55 +0900

    aa

--- 평가/js/a.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".tabmenu>dt").click(function(){
-        $(".tabMenu>dt").removeClass("on")
-        $(this).addClass("on")
-        $(".tabmenu>dd").removeClass("on")
-        $(this).next().addClass("on")
-    })
-})
